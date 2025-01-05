bdba630b5c32ead26bf095216a24e6ad32558462
rodrigopnonato <rodrigopnonato@gmail.com>
2025-01-05 14:56:10 -0300

    fix: Ajustado compatibilidade com telas menores, adaptando o menu de navegação e conteúdo no main

--- financas-pessoais/resources/views/layouts/main.blade.php
+++ financas-pessoais/resources/views/layouts/main.blade.php
@@ -8,19 +8,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"/>
         <link rel="stylesheet" href="/css/style.css">
-        
-        <!-- Bootstrap JS (com o Popper incluído) -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     </head>
 
     <body>
         @include('layouts.header')
 
-        <div class="content-fluid">
+        <div class="container-fluid">
             <div class="row">
                 <nav class="col-md-3 col-lg-2 d-md-block bg-light sidebar py-4 d-none d-md-block">
                     <div class="position-sticky">
@@ -28,7 +24,7 @@
                     </div>
                 </nav>
 
-                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 fs-4 fs-md-3 fs-lg-2">
+                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 fs-5 fs-sm-4 fs-md-3 fs-lg-2">
                     <div class="content py-4">
                         @include('layouts.message')
                         @yield('content')
@@ -40,10 +36,8 @@
         @include('layouts.footer')
 
         <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <!-- Bootstrap 5 JS -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 
 </html>
